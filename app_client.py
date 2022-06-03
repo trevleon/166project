f7516ac9200953eaf6cae8c0b20a0558f04457b0
rickyparada6 <ricky.6.parada@gmail.com>
2022-06-03 00:43:07 -0700

    z

--- app_client.py
+++ app_client.py
@@ -5,70 +5,67 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import math
 
-def get_blocks(n, T, m, epsilon, type):
-    #k = m + 2 * np.log2(T / epsilon)
-    #k = 
-    k = 2
-    blocks = []
-    types = ["SHANNON", "RENYI", "MIN"]
-    #for t in tqdm(types): 
-    blocks.append(entropy_stream.create_block_source(n, T, entropy_stream.compute_threshold(k, types[type]), types[type]))
-    
-    return blocks
-def get_blocks_probing(n, T, m, epsilon, type):
-    #k = 2
-    k = 4 * math.log2(m)
-    blocks = []
-    types = ["SHANNON", "RENYI", "MIN"]
-    #for t in tqdm(types): 
-    blocks.append(entropy_stream.create_block_source(n, T, entropy_stream.compute_threshold(k, types[type]), types[type]))
-    
+# types = ["SHANNON", "RENYI", "MIN"]
+types = ["4.6", "4.7", "4.10"]
 
+def get_block(N, M, T, epsilon, type):
+    blocks = [] 
+    p = entropy_stream.compute_threshold(M, T, epsilon, types[type])
+    print(p, ">?", 1 / N)
+    # for t in tdqm(types):
+    return entropy_stream.create_block_source(N, T, p, types[type])
+
+"""
+def get_blocks_probing(N, T, M, epsilon, type):
+    #k = 2
+    k = 4 * math.log2(M)
+    blocks = []
+    types = ["SHANNON", "RENYI", "MIN"]
+    #for t in tqdm(types): 
+    p = entropy_stream.compute_threshold(M, T, epsilon, types[type])
+    blocks.append(entropy_stream.create_block_source(N, T, p, types[type]))
+    return blocks
+"""
 
 def test_probing():
     print("Testing Linear Probing insertions to half capacity")
-    blocks = get_blocks(10, 10, 20, .01)
-    for block in blocks:
-        table = apps.LinearProbing(20)
-        elems = entropy_stream.sample_block_source(block)
-        for elem in elems:
-            table.insert(elem)
-        print("load factor: ", table.get_load() / table.get_size())
-
+    block = get_block(10, 10, 20, .01, 1)
+    table = apps.LinearProbing(20)
+    elems = entropy_stream.sample_block_source(block)
+    for elem in elems:
+        table.insert(elem)
+    print("load factor: ", table.get_load() / table.get_size())
 
 def test_allocation():
     print("Testing Balanced Allocation")
-    blocks = get_blocks(10, 10, 20, .1)
-    for block in blocks:
-        # not really sure how to use/test this one, but here's an
-        # initialized class with m=20, d=2
-        var_name = apps.BalancedAllocation(20, 2)
-        # elems is an array of 10 elements to hash, of the form
-        # [7. 6. 2. 7. 8. 7. 5. 4. 5. 3.] 
-        elems = entropy_stream.sample_block_source(block)
+    block = get_block(10, 10, 20, .1, 1) 
+    # not really sure how to use/test this one, but here's an
+    # initialized class with m=20, d=2
+    var_name = apps.BalancedAllocation(20, 2)
+    # elems is an array of 10 elements to hash, of the form
+    # [7. 6. 2. 7. 8. 7. 5. 4. 5. 3.] 
+    elems = entropy_stream.sample_block_source(block)
 
 def test_filter():
     print("Testing Bloom Filter")
-    blocks = get_blocks(10, 10, 20, .1)
-    for block in blocks:
-        # not really sure how to use/test this one either, but here's an
-        # initialized class with m=20, l=2
-        var_name = apps.BloomFilter(20, 2)
-        # elems is an array of 10 elements to hash, of the form
-        # [7 6 2 7 8 7 5 4 5 3] 
-        elems = entropy_stream.sample_block_source(block)
+    blocks = get_blocks(10, 10, 20, .1, 1) 
+    # not really sure how to use/test this one either, but here's an
+    # initialized class with m=20, l=2
+    var_name = apps.BloomFilter(20, 2)
+    # elems is an array of 10 elements to hash, of the form
+    # [7 6 2 7 8 7 5 4 5 3] 
+    elems = entropy_stream.sample_block_source(block)
 
-def linear_probing_data(t): 
+def linear_probing_data(t):
     datas = []
     for _ in range(1): 
-        blocks = get_blocks(200,200,20,.01, t)
-        for block in (blocks):
-            data = [] 
-            table = apps.LinearProbing(200)
-            elems = entropy_stream.sample_block_source(block)
-            for elem in elems: 
-                data.append(table.insert(elem))
-            datas.append(data)
+        block = get_block(1000000, 20, 19, 1 / (19**1.0001), t) 
+        data = []
+        table = apps.LinearProbing(200)
+        elems = entropy_stream.sample_block_source(block)
+        for elem in elems: 
+            data.append(table.insert(elem))
+        datas.append(data)
     return datas
     ret = []
     for i in range(len(datas[0])):
@@ -77,51 +74,44 @@ def linear_probing_data(t):
             sum += d[i]
         ret.append(sum / len(datas[0]))
     return ret
+
 def balanced_data(t): 
     datas = []
     for _ in range(10): 
         blocks = get_blocks(10,100,20,.01, t)
-
-        for block in blocks: 
-            data = []
-            table = apps.BalancedAllocation(200)
-            elems = entropy_stream.sample_block_source(block)
-            for elem in elems: 
-                table.insert(elem)
-                data.append(table.get_median_load())
-            datas.append(data)
+        data = []
+        table = apps.BalancedAllocation(200)
+        elems = entropy_stream.sample_block_source(block)
+        for elem in elems: 
+            table.insert(elem)
+            data.append(table.get_median_load())
+        datas.append(data)
     return datas
+
 def bloom_data(t): 
     datas = []
     for _ in range(10):
         blocks = get_blocks(10,100,20,.01, t)
-
-        for block in blocks: 
-            data = []
-            elems_1 = entropy_stream.sample_block_source(block)
-            elems_2 = entropy_stream.sample_block_source(block)
-            bloom = apps.BloomFilter(200)
-            for elem in elems_1: 
-                bloom.insert(elem)
-            fp_count = 0
-            for elem in elems_2: 
-                if elem not in elems_1 and bloom.contains(elem): 
-                    fp_count += 1
-            data.append(fp_count)
+        data = []
+        elems_1 = entropy_stream.sample_block_source(block)
+        elems_2 = entropy_stream.sample_block_source(block)
+        bloom = apps.BloomFilter(200)
+        for elem in elems_1: 
+            bloom.insert(elem)
+        fp_count = 0
+        for elem in elems_2: 
+            if elem not in elems_1 and bloom.contains(elem): 
+                fp_count += 1
+        data.append(fp_count)
         datas.append(data)
     return datas
-                
-
-
-
-
 
 def main():
     #test_probing()
     #test_allocation()
     #test_filter()
-    data = linear_probing_data(1)
-    x = [i for i in range(len(data))] 
+    data = linear_probing_data(2)
+    x = [i for i in range(len(data))]
     plt.plot(x, data[0])
     plt.show()
 
--- entropy_stream.py
+++ entropy_stream.py
@@ -2,21 +2,29 @@ import random
 import numpy as np
 from numpy import linalg as LA
 
-def compute_threshold(k, entropy, n, epsilon):
-    if entropy == "SHANNON":
-        return max(min(k, 1), 1 / n + epsilon)
-    elif entropy == "RENYI" or entropy == "MIN":
-        return max(min(2 ** (-k), 1), 1 / n + epsilon)
+def compute_threshold(M, T, epsilon, type):
+    """
+    returns p value for given choice of entropy
+    """
+    if type == "4.6":
+        K = (M * T ** 2) / (4 * epsilon ** 2)
+        return 1 / K
+    elif type == "4.7":
+        K = (M * T ** 2) / epsilon
+        return 1 / M + T / (epsilon * K)
+    elif type == "4.10":
+        K = (M * T ** 2) / epsilon
+        return 1 / M + 1 / K + np.sqrt((2 * T) / (epsilon * M)) * (1 / K)
     else:
         raise NotImplemented("passed in invalid entropy type")
 
-def create_block_source(N, T, k, e_type):
+def create_block_source(N, T, p, e_type):
     """
     T: size of random variables sequence in block
-    k: threshold where
-        - min: k = max prob
-        - renyi: k = collision prob
-        - shannon: k = min threshold entropy
+    p: threshold denoting described probability per block
+        - min: p = max prob
+        - renyi: p = collision prob
+        - shannon: p = min threshold entropy
     e_type: either min, Renyi, Shannon
 
     Return: a 2d array of size TxN where row i represents the distribution of X_i
@@ -29,14 +37,11 @@ def create_block_source(N, T, k, e_type):
         dist += np.random.uniform(0, 1, N)
         dist /= np.sum(dist)
         # print("distribution: ", dist)
-        # print("collision probability: ", LA.norm(dist)**2)
-        
-        if (e_type == "RENYI" and LA.norm(dist)**2 <= k) or \
-           (e_type == "MIN" and np.max(dist) <= k) or \
-           (e_type == "SHANNON" and np.sum(dist * np.log(1/dist)) < k):
-                prev_dist = dist
-                block = np.vstack((block, dist)) 
-                t += 1     
+        # print("collision probability: ", LA.norm(dist)**2) 
+        if ((e_type == "4.6" or e_type == "4.7" or e_type == "4.10") and LA.norm(dist)**2 <= p): 
+            prev_dist = dist
+            block = np.vstack((block, dist)) 
+            t += 1     
         else:
             dist = prev_dist
     return block[1:]
